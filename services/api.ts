a765812c24f4e511bc0b56050fde2db34358be8e
Sonam Agarwal <Sonamag1706@gmail.com>
2026-02-15 08:27:08 +0530

    home-screen-ui

---
+++ services/api.ts
@@ -0,0 +1,15 @@
+// const url =
+//   "https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGE2NjQ0MTQ5OWFmOGQ3ZWFmZmY3YzZhYWYzMjFhMiIsIm5iZiI6MTc3MTA4NDI3NS4zNzQsInN1YiI6IjY5OTA5OWYzNjM4NTYzYmNmN2VmYWNiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6XCtgiKThejWCfGKL4FoEGxsKA7eq2d10IAAHjjxXT8",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
